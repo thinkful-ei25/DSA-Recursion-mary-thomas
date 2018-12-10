e7bc4c6ec133bd4228b3846c8330e10f75140a6b
ThomasChun <tchun2055@gmail.com>
2018-12-10 10:30:56 -1000

    added array double

--- Thomas-Mary-Recursion-Drills.js
+++ Thomas-Mary-Recursion-Drills.js
@@ -1,3 +1,4 @@
+'use strict';
 // function countSheep(nOSheep) {
 //     if (nOSheep === 0) {
 //        console.log('No sheep left!')
@@ -10,24 +11,45 @@
 
 // countSheep(3);
 
-// function doubleArray(array){
-// if(array.length===0){
-//     return;
-// }
-//     return (array[0] * 2);
+
+
+// function countSheep(nOSheep) {
+//     if (nOSheep === 0) {
+//         return 'No sheep left!';
+//     } else {
+//         console.log(nOSheep)
+//         countSheep(--nOSheep);
+//     }
 // }
 
-// console.log(doubleArray([1,2,3]));
+// countSheep(3);
 
-function countSheep(nOSheep) {
-    if (nOSheep === 0) {
-        return 'No sheep left!';
-    } else {
-        console.log(nOSheep)
-        countSheep(--nOSheep);
-    }
+// return num1+ (recursion(num1+1, num2)
+
+
+
+// Array Double
+function doubleArray(array) {
+  if (array.length === 0) {
+    return [];
+  }
+  return [array[0]*2, ...doubleArray(array.slice(1))];
 }
+console.log(doubleArray([1, 2, 3]));
 
-countSheep(3);
+// if you want to use push
+function some(arr, newArr) { newArr = newArr || []; }
 
-return num1+ (recursion(num1+1, num2)
+
+
+// Array Double Iterative
+// function doubleArray(array) {
+//   let newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     let currentPlace = array[i];
+//     newArray.push(currentPlace*2);
+//   }
+//   return newArray;
+// }
+
+// console.log(doubleArray([1, 2, 3]));
